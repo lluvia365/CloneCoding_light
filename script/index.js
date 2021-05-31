5de3c3b59e4fdf7d9d216014581f67ebf53b4328
lluvia365 <lluvia365@gmail.com>
2021-05-31 16:27:29 +0900

    right

--- script/index.js
+++ script/index.js
@@ -9,22 +9,42 @@ $(function(){
         $(this).children('.subMenu').stop().slideUp();
     });
     
-});
 
+    // secC
+    $('.button').click(function(){
+        $('.button').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+    // 공지사항 버튼 클릭할 때
+    $('#btn1').click(function(){
+        $('#gallery').hide();
+        $('#notice').show();
+    });
+
+    // 갤러리 버튼 클릭할 때
+    $('#btn2').click(function(){
+        $('#gallery').show();
+        $('#notice').hide();
+    });
+
+    
+    
+});  //$(function()
 
 
 var slideNo=0;
 
 function slide(){
-    slideNo++
-    if(slide>2) slideNo=0
+    slideNo++;
+    if(slideNo>2) slideNo=0;
     
     $('.slide').fadeOut(1000);
     
     $('.slide').eq(slideNo).fadeIn(1000);
     
 }
-
 
 function playSlide(){
     setInterval('slide()',3000);
