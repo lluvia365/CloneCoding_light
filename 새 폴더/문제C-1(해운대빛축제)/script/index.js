a32f4693ee9c7e4cf503ff6e9c3691ffc22cebed
lluvia365 <lluvia365@gmail.com>
2021-06-01 11:01:02 +0900

    1

--- 새 폴더/문제C-1(해운대빛축제)/script/index.js
+++ 새 폴더/문제C-1(해운대빛축제)/script/index.js
@@ -25,6 +25,25 @@ $(function(){
         $(this).children('.subMenu').stop().slideUp();
     })
     
+    
+    $('.btn2').click(function(){
+        $('#notice').hide();
+        $('#gallery').css('display','flex');
+        
+        $('.btn1').removeClass('active');
+        $('.btn2').addClass('active');
+    })
+       
+    $('.btn1').click(function(){
+        $('#notice').show();
+        $('#gallery').hide();
+        
+        $('.btn1').addClass('active');
+        $('.btn2').removeClass('active');
+    })
+    
+    
+    
 });  // $(function)
 
 
